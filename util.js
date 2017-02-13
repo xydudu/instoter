8b925501609ee9b3d80fcdf580f4d8888169387c
xydudu <xuedudu@gmail.com>
2017-02-14 00:19:42 +0800

    media to media_data

--- util.js
+++ util.js
@@ -74,7 +74,7 @@ class Util {
 
     static uploadPhotos(_photo, _client) {
         return new Promise((_resolve, _reject) => {
-            _client.post('media/upload', {media: _photo}, (_err, _body, _res) => {
+            _client.post('media/upload', {media_data: _photo}, (_err, _body, _res) => {
                 if (_err) return _reject(_err) 
                 _resolve(_body)
             })
